7781a7078fa970cc82eb47ce4f60a3e469e24bf7
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-10-23 12:48:13 +0300

    функция createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,28 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+  let xhr = new XMLHttpRequest;
+  xhr.withCredentials = true;
+  if (options.method === 'GET') {
+    options.url = options.url + '?mail=' + options.data.mail + '&password=' + options.data.password;
+    xhr.open(options.method, options.url)
+    xhr.send()
+  } else {
+    let formData = new FormData;
+    formData.append('mail', options.data.mail);
+    formData.append('password', options.data.password);
+    xhr.open(options.method, options.url);
+    xhr.send(formData);
+  }
+  xhr.responeType = options.responseType;
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      err = null;
+      options.callback(err, xhr.response);
+    } else {
+      err = xhr.statusText;
+      options.callback(err);
+    }
+  }
+  return xhr;
 };
